d0e37aad7c26df56740fabcc777ce9ed6f23bb5f
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-10-26 17:14:01 +0000

    Add update & delete function

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 '''Command Line Interpreter'''
 import cmd
 from models import storage
-from models import base_model, FileStorage
+from models import *
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
@@ -14,15 +15,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, *args):
         '''Quit command to exit the program'''
-        return True
+        quit()
     
     def do_create(self, class_name):
         '''Create a new instance called <name>'''
         if class_name:
-            if class_name == "BaseModel":
-                class_name = base_model.BaseModel()
-                class_name.save()
-                object_dict = class_name.__dict__
+            if class_name in self.classes_list:
+                class_instance = eval(class_name)()
+                class_instance.save()
+                object_dict = class_instance.__dict__
                 print(object_dict['id'])
             else:
                 print("** class doesn't exist **")
@@ -46,14 +47,6 @@ class HBNBCommand(cmd.Cmd):
                 if class_name in self.classes_list:
                     # check if instance_id exists
                     key = f"{class_name}.{instance_id}"
-                    # object_received = storage.all()
-
-                    # for obj_id in object_received.keys():
-                    #     if obj_id == key:
-                    #         obj = object_received[obj_id]
-                    #         print(obj)
-                    #         return
-                    # print("** no instance found **")
                     if key not in storage.all():
                         print("** no instance found **")
                     else:
@@ -64,6 +57,79 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print("** class doesn't exist **")
 
+    def do_delete(self, line):
+        # check if class name and instance id was provided
+        if line == "" or line is None:
+            print("** class name missing **")
+        
+        else:
+            # get all the arguments passed via the command line
+            class_info = line.split(" ")
+            if len(class_info) < 2:
+                print("** instance id missing **")
+            else:
+                class_name = class_info[0]
+                instance_id = class_info[1]
+                # check if class name exists
+                if class_name in self.classes_list:
+                    # check if instance_id exists
+                    key = f"{class_name}.{instance_id}"
+                    if key not in storage.all():
+                        print("** no instance found **")
+                    else:
+                        # delete this instance and save to json
+                        del storage.all()[key]
+                        storage.save()
+                        return
+                        
+                    
+                else:
+                    print("** class doesn't exist **")
+
+    def do_all(self, line):
+        instance_obj = storage.all()
+        instance_list = []
+        
+        if line:
+                if line not in self.classes_list:
+                    print("** class doesn't exist **")
+                    return
+                else:
+                    for key in instance_obj.keys():
+                         # split key into class name and instance id
+                        class_name, instance_id = key.split(".")
+                        if line == class_name:
+                            instance_dict = instance_obj[key]
+                            # # print the str representation of the instance
+                            instance_list.append(str(f"[{class_name}] ({instance_id}) {str(instance_dict)}"))
+                            print(instance_list)
+                            
+        else:
+            # grab each intance and format for printing
+            for key in instance_obj.keys():
+                #  check if class was provided
+                # if line is not "" or line is not None:
+
+                # split key into class name and instance id
+                class_name, instance_id = key.split(".")
+                # print(class_name, instance_id)
+                instance_dict = instance_obj[key]
+                # # print the str representation of the instance
+                instance_list.append(str(f"[{class_name}] ({instance_id}) {str(instance_dict)}"))
+            print(instance_list)
+
+    def do_update(self, line):
+        (class_name, instance_id, attribute, value) = line.split(" ")
+        print(class_name, instance_id, attribute, value)
+
+        key = f"{class_name}.{instance_id}"
+        if key not in storage.all():
+            print("** no instance found **")
+        else:
+            instance_dict = storage.all()[key]
+            # update attributes in the instance dictionary
+            instance_dict[attribute] = value
+            storage.save()
 
     def emptyline(self):
         pass
--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''Initializes the package'''
-from models.engine.file_storage import FileStorage
+# from models.base_model import BaseModel
+from models.engine import file_storage
 
 
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()
